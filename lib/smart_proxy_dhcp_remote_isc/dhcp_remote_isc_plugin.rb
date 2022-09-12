ff2d927ff94d3b27c7f4d476fd1aed2f9991b112
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-09-12 16:28:41 +0200

    Fix Layout/SpaceAroundOperators cop

--- lib/smart_proxy_dhcp_remote_isc/dhcp_remote_isc_plugin.rb
+++ lib/smart_proxy_dhcp_remote_isc/dhcp_remote_isc_plugin.rb
@@ -4,7 +4,7 @@ module Proxy::DHCP::RemoteISC
 
     requires :dhcp, '>= 1.16'
     default_settings :config => '/etc/dhcp/dhcpd.conf', :leases => '/var/lib/dhcpd/dhcpd.leases',
-                     :omapi_port => '7911', :blacklist_duration_minutes => 30*60
+                     :omapi_port => '7911', :blacklist_duration_minutes => 30 * 60
 
     validate_readable :config, :leases
 
